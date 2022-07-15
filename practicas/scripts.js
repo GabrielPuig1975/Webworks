51080ff4cf1264bb08573093b3b1ebad267c50d5
GabrielPuig1975 <99985368+GabrielPuig1975@users.noreply.github.com>
2022-07-14 23:11:40 -0300

    Delete practicas directory

--- practicas/scripts.js
+++
@@ -1,7 +0,0 @@
-const cod = document.getElementById('cod');
-const desc = document.getElementById('desc');
-const tipo = document.getElementById('tipo');
-const select = document.getElementById('select');
-const stock = document.getElementById('stock');
-const pmy = document.getElementById('pmy');
-const pcp = document.getElementById('pcp');
